a3a1fafea61978b6549ec581a10ecf76b1eece05
Navil Rodrigues <rodriguesnavil@gmail.com>
2023-08-23 23:22:34 +0530

    setup: Initial refined boilerplate

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,25 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
 
 // The next line is part of the sample project, you don't need it in your
 // project. It imports a Hardhat task definition, that can be used for
 // testing the frontend.
 require("./tasks/faucet");
 
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "https://eth-sepolia.g.alchemy.com/v2/YOUR-API-KEY"
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      saveDeployments: true,
+      chainId: 11155111,
+      blockConfirmation: 6,
+    },
+  },
   solidity: "0.8.17",
 };
